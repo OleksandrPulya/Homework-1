f723689a018bdc54755c07fd42a74bd5124c362c
Oleksandr Pulya <80588959+OleksandrPulya@users.noreply.github.com>
2025-10-20 21:06:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const images = document.querySelectorAll('.city__image img');
 images.forEach(img => {
   img.addEventListener('mousemove', e => {
     const rect = e.target.getBoundingClientRect();
-    const x = ((e.clientX - rect.left) / rect.width - 0.5) * 10; // нахил по X
-    const y = ((e.clientY - rect.top) / rect.height - 0.5) * 10; // нахил по Y
-    img.style.transform = `scale(1.2) translate(${x}px, ${y}px)`; // збільшення + рух
+    const x = ((e.clientX - rect.left) / rect.width - 0.5) * 10;
+    const y = ((e.clientY - rect.top) / rect.height - 0.5) * 10;
+    img.style.transform = `scale(1.2) translate(${x}px, ${y}px)`;
   });
 
   img.addEventListener('mouseleave', () => {
-    img.style.transform = 'scale(1) translate(0,0)'; // повертаємось у норму
+    img.style.transform = 'scale(1) translate(0,0)';
   });
 });
